dee3a3dc80753c47093c55be7cdb47324b56402a
Mikael Klages <ITR@live.no>
2023-02-21 04:48:43 +0100

    Imported super simple character controller

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+class InputController : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController2D _characterController;
+
+    private float _xDir;
+    private bool _crouch, _jump;
+
+    private void Reset()
+    {
+        _characterController = GetComponent<CharacterController2D>();
+    }
+
+    private void Update()
+    {
+        _xDir = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+        _crouch = y < 0 || Input.GetKey(KeyCode.LeftShift);
+        _jump = y > 0 || Input.GetKey(KeyCode.Space);
+    }
+
+    private void FixedUpdate()
+    {
+        _characterController.Move(_xDir, _crouch, _jump);
+    }
+}
