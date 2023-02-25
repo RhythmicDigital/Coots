2d444658508c6b335682eeb29921e0d3788ca1cc
Al3xanderL3e <76575471+Al3xanderL3e@users.noreply.github.com>
2023-02-24 21:59:03 -0500

    Fixed projectiles shooting in wrong direction

--- Assets/Scripts/Core/ProjectileShooter.cs
+++ Assets/Scripts/Core/ProjectileShooter.cs
@@ -85,6 +85,7 @@ public class ProjectileShooter : MonoBehaviour
         projectile.SetActive(true);
         projectile.SetPosition(transform.position);
         projectile.SetMoveSpeed(projectileSpeed);
+        projectile.SetRotation(0);
 
         if (targetPlayer)
             projectile.SetMoveDirection(GameController.i.Player.transform.position - transform.position);
